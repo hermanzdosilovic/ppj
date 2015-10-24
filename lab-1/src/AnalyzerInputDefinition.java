f8f3be05a769c60d37de8c6fc733624b76d3a3ad
Ivan Trubić <itrubic.truba@gmail.com>
2015-10-24 20:46:34 +0200

    Added basic code outline

--- lab-1/src/AnalyzerInputDefinition.java
+++ lab-1/src/AnalyzerInputDefinition.java
@@ -1,10 +1,31 @@
 
 /**
- * Class for lexical analyzer definition with a specific input format
+ * Class for lexical analyzer definition with a specific input format.
  * 
  * @author Truba
  *
  */
 public class AnalyzerInputDefinition {
+	
 
+	public AnalyzerInputDefinition(){
+		
+		
+	}
+	
+	public void regularDefinitionInput(){
+		
+	}
+	
+	public void lexicalStateDefinition(){
+		
+	}
+	
+	public void lexicalNameDefinition(){
+		
+	}
+	
+	public void lexicalAnalyzerRulesDefinition(){
+		
+	}
 }
