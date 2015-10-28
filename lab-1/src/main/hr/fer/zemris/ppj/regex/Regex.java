7c708df492899d78b7435c48e92b744975e324ee
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-10-28 17:40:18 +0100

    Added subExpressions method

--- lab-1/src/main/hr/fer/zemris/ppj/regex/Regex.java
+++ lab-1/src/main/hr/fer/zemris/ppj/regex/Regex.java
@@ -1,23 +1,67 @@
 package hr.fer.zemris.ppj.regex;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.fer.zemris.ppj.automaton.Automaton;
+
 public class Regex {
   
-  private String value;
+  private String expression;
+  private Automaton automaton;
   
   public Regex(final String value) {
-    this.value = value;
+    this.expression = value;
   }
 
-  public String getValue() {
-    return value;
+  public String getExpression() {
+    return expression;
   }
   
-  public boolean isEscapedCharacterAt(int index) {
+  public Automaton toAutomaton() {
+    if (automaton != null) {
+      return automaton;
+    }
+    automaton = new Automaton();
+//    convertExpressionToAutomaton(expression);
+    return automaton;
+  }
+  
+  public List<String> getSubExpressions() {
+    return getSubExpressions(expression);
+  }
+  
+  private List<String> getSubExpressions(String expression) {
+    List<String> subExpressions = new ArrayList<>();
+    int numberOfParenthesis = 0;
+    int leftIndex = 0;
+    
+    for (int i = 0; i < expression.length(); i++) {
+      char character = expression.charAt(i);
+      if (character == '(' && !escapedCharacterAt(expression, i)) {
+        numberOfParenthesis++;
+      } else if (character == ')' && !escapedCharacterAt(expression, i)) {
+        numberOfParenthesis--;
+      } else if (numberOfParenthesis == 0 && character == '|' && !escapedCharacterAt(expression, i)) {
+        subExpressions.add(expression.substring(leftIndex, i));
+        leftIndex = i + 1;
+      }
+    }
+    subExpressions.add(expression.substring(leftIndex));
+    
+    return subExpressions;
+  }
+  
+  private boolean escapedCharacterAt(String value, int index) {
     boolean isEscaped = false;
     while(index - 1 >= 0 && value.charAt(index - 1) == '\\') {
       isEscaped = !isEscaped;
       index--;
     }
     return isEscaped;
+  }
+  
+  public boolean escapedCharacterAt(int index) {
+    return escapedCharacterAt(expression, index);
   }
 }
--- lab-1/src/test/hr/fer/zemris/ppj/automaton/regex/RegexTest.java
+++ lab-1/src/test/hr/fer/zemris/ppj/automaton/regex/RegexTest.java
@@ -1,7 +1,10 @@
 package hr.fer.zemris.ppj.automaton.regex;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
 
 import hr.fer.zemris.ppj.regex.Regex;
 
@@ -11,13 +14,57 @@ public class RegexTest {
   public void isOperatorTest() {
     //                       012-3-456-7-8-901-2-3-4-56
     Regex regex = new Regex("a|b\\\\cd\\\\\\ef\\\\\\\\|"); // a|b\\cd\\\|f\\\\|
-    assertEquals(false, regex.isEscapedCharacterAt(0)); // a
-    assertEquals(false, regex.isEscapedCharacterAt(1)); // |
-    assertEquals(false, regex.isEscapedCharacterAt(2)); // b
-    assertEquals(true, regex.isEscapedCharacterAt(4)); // \
-    assertEquals(false, regex.isEscapedCharacterAt(5)); // c
-    assertEquals(true, regex.isEscapedCharacterAt(10)); // |
-    assertEquals(false, regex.isEscapedCharacterAt(11)); // f
-    assertEquals(false, regex.isEscapedCharacterAt(16)); // |
+    assertFalse(regex.escapedCharacterAt(0)); // a
+    assertFalse(regex.escapedCharacterAt(1)); // |
+    assertFalse(regex.escapedCharacterAt(2)); // b
+    assertTrue(regex.escapedCharacterAt(4)); // \
+    assertFalse(regex.escapedCharacterAt(5)); // c
+    assertTrue(regex.escapedCharacterAt(10)); // |
+    assertFalse(regex.escapedCharacterAt(11)); // f
+    assertFalse(regex.escapedCharacterAt(16)); // |
+  }
+  
+  @Test
+  public void getSubExpressionsTest() {
+    Regex regex = new Regex("a|b");
+    List<String> subExpressions = regex.getSubExpressions();
+    assertEquals(2, subExpressions.size());
+    assertEquals("a", subExpressions.get(0));
+    assertEquals("b", subExpressions.get(1));
+    
+    regex = new Regex("a");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(1, subExpressions.size());
+    assertEquals("a", subExpressions.get(0));
+    
+    regex = new Regex("(a|b)|(cd)|ef");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(3, subExpressions.size());
+    assertEquals("(a|b)", subExpressions.get(0));
+    assertEquals("(cd)", subExpressions.get(1));
+    assertEquals("ef", subExpressions.get(2));
+    
+    regex = new Regex("(a|b(cdef|))");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(1, subExpressions.size());
+    assertEquals("(a|b(cdef|))", subExpressions.get(0));
+    
+    regex = new Regex("a||b");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(3, subExpressions.size());
+    assertEquals("a", subExpressions.get(0));
+    assertEquals("", subExpressions.get(1));
+    assertEquals("b", subExpressions.get(2));
+    
+    regex = new Regex("a\\|b");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(1, subExpressions.size());
+    assertEquals("a\\|b", subExpressions.get(0));
+    
+    regex = new Regex("a\\|b\\\\|c");
+    subExpressions = regex.getSubExpressions();
+    assertEquals(2, subExpressions.size());
+    assertEquals("a\\|b\\\\", subExpressions.get(0));
+    assertEquals("c", subExpressions.get(1));
   }
 }
