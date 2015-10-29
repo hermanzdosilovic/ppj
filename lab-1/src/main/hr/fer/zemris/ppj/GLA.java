a88d6b30a9390aa20ee19edd19505dc7faa0f2b8
Ivan Trubić <itrubic.truba@gmail.com>
2015-10-29 15:26:05 +0100

    neke promjenice vamo tamo

--- lab-1/src/main/hr/fer/zemris/ppj/GLA.java
+++ lab-1/src/main/hr/fer/zemris/ppj/GLA.java
@@ -3,11 +3,15 @@ package hr.fer.zemris.ppj;
 import hr.fer.zemris.ppj.regex.Regex;
 
 import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public final class GLA {
   public static void main(String[] args) throws Exception {
     System.setIn(new FileInputStream("/home/truba/workspace/ppj/in.txt"));
+
+    PrintWriter writer = new PrintWriter("file.txt");
+
     GeneratorInputDefinition inputDefinition = new GeneratorInputDefinition();
     Map<String, LexicalAnalyzerState> rules = inputDefinition.getLexicalState();
     LexicalAnalyzerState initialState = inputDefinition.getInitialAnalyzerState();
@@ -16,9 +20,12 @@ public final class GLA {
     for(Map.Entry<String, LexicalAnalyzerState> entry : rules.entrySet()){
       LexicalAnalyzerState state = entry.getValue();
       for(RegexAction action : state.getRegexActionList()){
-        System.out.println(state.getName());
-        System.out.println((new Regex(action.getRegex())).toAutomaton());
+        writer.println("%");
+        writer.println(state.getName());
+        writer.println((new Regex(action.getRegex())).toAutomaton());
+        writer.println("%");
       }
+     writer.close();
       
     }
   }
