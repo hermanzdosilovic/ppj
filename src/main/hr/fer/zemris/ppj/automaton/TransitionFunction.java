e1cc1003aa5d652ce4676163ef1eeeac81009221
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-11-11 19:00:03 +0100

    Added transition function implementation

---
+++ src/main/hr/fer/zemris/ppj/automaton/TransitionFunction.java
@@ -0,0 +1,42 @@
+package hr.fer.zemris.ppj.automaton;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import hr.fer.zemris.ppj.Pair;
+
+/**
+ * 
+ * @author Herman Zvonimir Dosilovic
+ *
+ * @param <S> source/destination
+ * @param <C> input symbol
+ */
+public class TransitionFunction<S, C> {
+
+  private Map<Pair<S, C>, Collection<S>> transitionTable;
+
+  public boolean addTransition(S source, C input, S destination) {
+    Pair<S, C> pair = new Pair<>(source, input);
+    if (!transitionTable.containsKey(pair)) {
+      transitionTable.put(pair, new ArrayList<>());
+    }
+    if (!transitionTable.get(pair).contains(destination)) {
+      return false;
+    }
+    return transitionTable.get(pair).add(destination);
+  }
+  
+  public Collection<S> getTransitionStates(S source, C input) {
+    Pair<S, C> pair = new Pair<>(source, input);
+    if (!transitionTable.containsKey(pair)) {
+      return null;
+    }
+    return transitionTable.get(pair);
+  }
+  
+  public boolean existsTransition(S source, C input) {
+    return getTransitionStates(source, input) != null;
+  }
+}
