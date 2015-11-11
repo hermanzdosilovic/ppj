347b884e6c05a11a50d882b5a506539a082e07f2
Ivan Trubić <itrubic.truba@gmail.com>
2015-11-11 19:59:07 +0100

    basic skeleton

---
+++ src/main/hr/fer/zemris/ppj/lab2/GeneratorInputDefinition.java
@@ -0,0 +1,25 @@
+package hr.fer.zemris.ppj.lab2;
+
+public class GeneratorInputDefinition {
+  
+  public GeneratorInputDefinition(){
+    
+  }
+  
+  public void startGenerator(){
+    
+  }
+  
+  private void parseNonterminalSymbols(){
+    
+  }
+  
+  private void parseTerminalSymbols(){
+    
+  }
+  
+  private void parseSynchronousTerminalSymbols(){
+    
+  }
+
+}
