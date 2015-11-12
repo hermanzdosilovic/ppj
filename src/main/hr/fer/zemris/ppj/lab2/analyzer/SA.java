50f42249bae7d7d7ff0f3e35f5a3368848d3db95
Josipa Kelava <kelava.josipa@gmail.com>
2015-11-12 23:30:32 +0100

    Dodan oporavak od pogreske

--- src/main/hr/fer/zemris/ppj/lab2/analyzer/SA.java
+++ src/main/hr/fer/zemris/ppj/lab2/analyzer/SA.java
@@ -5,6 +5,7 @@ import hr.fer.zemris.ppj.lab2.parser.action.AcceptAction;
 import hr.fer.zemris.ppj.lab2.parser.action.Action;
 import hr.fer.zemris.ppj.lab2.parser.action.MoveAction;
 import hr.fer.zemris.ppj.lab2.parser.action.ReduceAction;
+import hr.fer.zemris.ppj.lab2.parser.action.RejectAction;
 import hr.fer.zemris.ppj.node.Node;
 
 import java.io.FileInputStream;
@@ -15,90 +16,126 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class SA {
 
-  private List<String> input;
+ 
   private Map<Pair<Integer, String>, Action> actions;
   private Map<Pair<Integer, String>, MoveAction> newState;
   private List<String> synStrings;
   private Integer startState;
-  private Node root;
-  
+  public static String epsilon = "$";
+
   public static void main(String[] args) {
 
-    FileInputStream fileIn = new FileInputStream("  ");
-    ObjectInputStream in = new ObjectInputStream(fileIn);
-    Map<Pair<Integer, String>, Action> actions =
-        (Map<Pair<Integer, String>, Action>) in.readObject();
-
-
-
-    SA sa = new SA();
-
-
+    // FileInputStream fileIn = new FileInputStream("  ");
+    // ObjectInputStream in = new ObjectInputStream(fileIn);
+    // Map<Pair<Integer, String>, Action> actions =
+    // (Map<Pair<Integer, String>, Action>) in.readObject();
+    //
+    //
+    //  
   }
 
-  public SA(List<String> input, Map<Pair<Integer, String>, Action> actions,
+  public SA(Map<Pair<Integer, String>, Action> actions,
       Map<Pair<Integer, String>, MoveAction> newState, List<String> synStrings, Integer startState) {
-    this.input = input;
     this.actions = actions;
     this.newState = newState;
     this.synStrings = synStrings;
     this.startState = startState;
-    
-    input.add("&");
-  }
 
-  public void LR() {
+  }
+  
+
+  public Node LR(List<String> input) {
+
+    Node root = null;
     
-    Integer stanje = startState; 
     Deque<Integer> stackState = new ArrayDeque<Integer>();
     Deque<Node> stackValue = new ArrayDeque<Node>();
-    
+
     stackState.push(startState);
-    
-    for(int j = 0; j < input.size();) {
-      
-      String a = input.get(j);
+
+    for (int index = 0; index < input.size() && !stackState.isEmpty();) {
+
+      String a = input.get(index);
       String[] podjela = a.split(" ");
-        
-      Pair<Integer, String> pair = new Pair<Integer, String>(stanje, podjela[0]);
+
+      Pair<Integer, String> pair = new Pair<Integer, String>(stackState.peek(), podjela[0]);
       Node node = new Node(a);
-      
+
       Action action = actions.get(pair);
-       
-      if(action instanceof MoveAction) {
+
+      if (action instanceof MoveAction) {
         stackValue.push(node);
-        stanje = ((MoveAction) action).getState();
-        stackState.push(stanje);
-        j++;
-      } else if(action instanceof ReduceAction) {
-        
+       
+        stackState.push(((MoveAction) action).getState());
+        index++;
+      } else if (action instanceof ReduceAction) {
+
         int reduceNumber = ((ReduceAction) action).getHowMany();
         String leftSide = ((ReduceAction) action).getLeftHandSide();
-        
+
         Node parent = new Node(leftSide);
-        
-        for(int i = 0; i < reduceNumber; i++) {
+        if (reduceNumber == 0) {
+          parent.addChild(new Node(epsilon));
+        }
+        for (int i = 0; i < reduceNumber; i++) {
           stackState.pop();
           parent.addChild(stackValue.pop());
         }
-        stanje = stackState.peek();
-        Pair<Integer, String> pairNewState = new Pair<Integer, String>(stanje, leftSide);
+     
+        Pair<Integer, String> pairNewState = new Pair<Integer, String>(stackState.peek(), leftSide);
         MoveAction actionNewState = newState.get(pairNewState);
-        stanje = actionNewState.getState();
-        stackState.add(stanje);
+        stackState.add(actionNewState.getState());
         stackValue.add(parent);
-        
-      } else if(action instanceof AcceptAction) {
-        j++;
+
+      } else if (action instanceof AcceptAction) {
+        index++;
         root = stackValue.pop();
-        
+
       } else {
-        //nesto nevalja
-      }    
+        errorOutput(stackState, podjela);
+      
+        for (; index < input.size(); index++) {
+        
+          if (synStrings.contains(a)) {
+            a = input.get(index);
+            podjela = a.split(" ");
+           
+            pair = new Pair<Integer, String>(stackState.peek(), podjela[0]);
+            while(!stackState.isEmpty()) {
+              if(!(actions.get(pair) instanceof RejectAction)) {
+                break;
+              }
+              else {
+                stackState.pop();
+                stackValue.pop();
+              }
+            }
+            break;
+          }
+        }
+      }
     }
+    return root;
+  }
+  private void errorOutput( Deque<Integer> stackState, String[] podjela) {
+    System.err.println("Broj retka: " + podjela[1]);
+    
+
+    System.err.print("Znakovi koji ne bi izazvali pogresku:");
+    for (Map.Entry<Pair<Integer, String>, Action> entry : actions.entrySet()) {
+      if (entry.getKey().getFirst().equals(stackState.peek())
+          && !(entry.getValue() instanceof RejectAction)) {
+        System.err.print(" " + entry.getKey().getSecond());
+      }
+    }
+    System.err.println();
+
+    System.err.println(podjela[0] + " " + podjela[2]);
+
   }
 }
