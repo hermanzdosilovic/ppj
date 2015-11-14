b7bb798514458bcc20c2526250917daa1e326355
Ivan Trubić <itrubic.truba@gmail.com>
2015-11-14 19:57:37 +0100

    transition function converter

--- src/main/hr/fer/zemris/ppj/automaton/EpsilonNFAConverter.java
+++ src/main/hr/fer/zemris/ppj/automaton/EpsilonNFAConverter.java
@@ -1,28 +1,45 @@
 package hr.fer.zemris.ppj.automaton;
 
+import java.util.HashMap;
 import java.util.Set;
 
 public class EpsilonNFAConverter {
 
-  public static <S, C> Automaton<S, C> convertToNFA(Automaton<S, C> automata){
-  Set<S> states = automata.getStates();
-  Set<C> alphabet = automata.getAlphabet();
-  TransitionFunction<S, C> transitionFunction = automata.getTransitionFunction();
-  S initialState = automata.getInitialState();
-  Set<S> acceptableStates = automata.getAcceptableStates();
-  
-   for(S state : automata.epsilonClosure(initialState)){
-     if(automata.hasAcceptableState(state)){
-       acceptableStates.add(initialState);
-     }
-   }
+  public static <S, C> Automaton<S, C> convertToNFA(Automaton<S, C> automata) {
+    Set<S> states = automata.getStates();
+    Set<C> alphabet = automata.getAlphabet();
+    TransitionFunction<S, C> oldTransitionFunction =
+        new TransitionFunction<S, C>(automata.getTransitionFunction());
+    S initialState = automata.getInitialState();
+    Set<S> acceptableStates = automata.getAcceptableStates();
 
-   
+    for (S state : automata.epsilonClosure(initialState)) {
+      if (automata.hasAcceptableState(state)) {
+        acceptableStates.add(initialState);
+      }
+    }
+
+    TransitionFunction<S, C> newTransitionFunction = new TransitionFunction<S, C>();
     
-    
-    Automaton<S, C> nfaAutomata = new Automaton<S, C>(states, alphabet, transitionFunction, initialState, acceptableStates);
-    
+    for (S state : states) {
+      for (C input : alphabet) {
+        for(S transition : automata.epsilonClosure(state)){
+          newTransitionFunction.addTransition(state, input, transition);
+        }
+        for(S transition : oldTransitionFunction.getTransitionResult(state, input)){
+          for(S closure : automata.epsilonClosure(transition)){
+            newTransitionFunction.addTransition(state, input, closure);
+          }
+         
+        }
+      }
+    }
+
+
+    Automaton<S, C> nfaAutomata =
+        new Automaton<S, C>(states, alphabet, newTransitionFunction, initialState, acceptableStates);
+
     return nfaAutomata;
-    
+
   }
 }
