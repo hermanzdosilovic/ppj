7ff3ae202700dbfd1a2c9cbd16ef7e02d93431d6
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-11-17 17:49:53 +0100

    Added converters

---
+++ src/main/hr/fer/zemris/ppj/grammar/coverters/GrammarEpsilonNFAConverter.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.ppj.grammar.coverters;
+
+import hr.fer.zemris.ppj.automaton.Automaton;
+import hr.fer.zemris.ppj.grammar.Grammar;
+import hr.fer.zemris.ppj.lab2.parser.LRItem;
+import hr.fer.zemris.ppj.symbol.Symbol;
+
+public class GrammarEpsilonNFAConverter {
+  
+  public static Automaton<LRItem, Symbol<?>> convert(Grammar grammar) {
+    return null;
+  }
+}
