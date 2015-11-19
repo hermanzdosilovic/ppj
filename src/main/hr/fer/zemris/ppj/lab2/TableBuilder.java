9e78e56076a03d7849a098f400d728daa24b63fd
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-11-19 23:28:11 +0100

    Finished newStateBuild and tests

--- src/main/hr/fer/zemris/ppj/lab2/TableBuilder.java
+++ src/main/hr/fer/zemris/ppj/lab2/TableBuilder.java
@@ -13,38 +13,66 @@ import hr.fer.zemris.ppj.lab2.parser.LRItem;
 import hr.fer.zemris.ppj.lab2.parser.action.AcceptAction;
 import hr.fer.zemris.ppj.lab2.parser.action.Action;
 import hr.fer.zemris.ppj.lab2.parser.action.MoveAction;
+import hr.fer.zemris.ppj.lab2.parser.action.PutAction;
 import hr.fer.zemris.ppj.lab2.parser.action.ReduceAction;
 import hr.fer.zemris.ppj.symbol.NonTerminalSymbol;
 import hr.fer.zemris.ppj.symbol.Symbol;
 import hr.fer.zemris.ppj.symbol.TerminalSymbol;
 
 public class TableBuilder {
-  public static Map<Pair<Set<LRItem>, TerminalSymbol<?>>, Action> buildActionTable(Automaton<Set<LRItem>, Symbol<?>> automaton, Production initialProduction) {
-    TransitionFunction<Set<LRItem>, Symbol<?>> transitionFunction = automaton.getTransitionFunction();
-    
+  public static Map<Pair<Set<LRItem>, TerminalSymbol<?>>, Action> buildActionTable(
+      Automaton<Set<LRItem>, Symbol<?>> automaton, Production initialProduction) {
+    TransitionFunction<Set<LRItem>, Symbol<?>> transitionFunction =
+        automaton.getTransitionFunction();
+
     Map<Pair<Set<LRItem>, TerminalSymbol<?>>, Action> actionTable = new HashMap<>();
     for (Set<LRItem> state : automaton.getStates()) {
       for (LRItem item : state) {
         Symbol<?> symbol = item.getDotSymbol();
-        
-        if (!item.isComplete() && transitionFunction.existsTransition(state, symbol) && (symbol instanceof TerminalSymbol<?>)) {
-          Set<LRItem> destination = new ArrayList<>(transitionFunction.getTransitionResult(state, symbol)).get(0);
-          actionTable.put(new Pair<>(state, (TerminalSymbol<?>) symbol), new MoveAction<>(destination));
+
+        if (!item.isComplete() && transitionFunction.existsTransition(state, symbol)
+            && (symbol instanceof TerminalSymbol<?>)) {
+          Set<LRItem> destination =
+              new ArrayList<>(transitionFunction.getTransitionResult(state, symbol)).get(0);
+          actionTable.put(new Pair<>(state, (TerminalSymbol<?>) symbol),
+              new MoveAction<>(destination));
         } else if (item.isComplete() && !item.getProduction().equals(initialProduction)) {
           for (Symbol<?> itemSymbol : item.getTerminalSymbols()) {
-            actionTable.put(new Pair<>(state, (TerminalSymbol<?>) itemSymbol), new ReduceAction(item.getProduction()));
+            actionTable.put(new Pair<>(state, (TerminalSymbol<?>) itemSymbol),
+                new ReduceAction(item.getProduction()));
           }
         } else if (item.getProduction().equals(initialProduction)) {
-          actionTable.put(new Pair<>(state, (TerminalSymbol<?>) new ArrayList<>(item.getTerminalSymbols()).get(0)), new AcceptAction());
+          actionTable.put(
+              new Pair<>(state,
+                  (TerminalSymbol<?>) new ArrayList<>(item.getTerminalSymbols()).get(0)),
+              new AcceptAction());
         }
       }
     }
-    
+
     return actionTable;
   }
 
   public static Map<Pair<Set<LRItem>, NonTerminalSymbol<?>>, Action> buildNewStateTable(
       Automaton<Set<LRItem>, Symbol<?>> automaton) {
-    return null;
+    TransitionFunction<Set<LRItem>, Symbol<?>> transitionFunction =
+        automaton.getTransitionFunction();
+    Set<Set<LRItem>> states = automaton.getStates();
+    Set<Symbol<?>> alphabet = automaton.getAlphabet();
+
+    Map<Pair<Set<LRItem>, NonTerminalSymbol<?>>, Action> newStateTable = new HashMap<>();
+    for (Set<LRItem> state : states) {
+      for (Symbol<?> symbol : alphabet) {
+        if (transitionFunction.existsTransition(state, symbol)
+            && (symbol instanceof NonTerminalSymbol<?>)) {
+          Set<LRItem> destination =
+              new ArrayList<>(transitionFunction.getTransitionResult(state, symbol)).get(0);
+          newStateTable.put(new Pair<>(state, (NonTerminalSymbol<?>) symbol),
+              new PutAction<>(destination));
+        }
+      }
+    }
+
+    return newStateTable;
   }
 }
