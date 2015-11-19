8b855a07b6e34e1f4894c6f70ad2ff051a50a79f
Ivan Trubić <itrubic.truba@gmail.com>
2015-11-19 14:49:46 +0100

    transition function finder and tests

--- src/main/hr/fer/zemris/ppj/automaton/converters/NFAConverter.java
+++ src/main/hr/fer/zemris/ppj/automaton/converters/NFAConverter.java
@@ -5,49 +5,70 @@ import java.util.HashSet;
 import java.util.Set;
 
 import hr.fer.zemris.ppj.automaton.Automaton;
+import hr.fer.zemris.ppj.automaton.TransitionFunction;
 import hr.fer.zemris.ppj.helpers.SubsetHelper;
 
 public class NFAConverter {
 
-  public static <S, C> Automaton<S, C> convertToDFA(Automaton<S, C> automaton) {
-    
+  public static <S, C> Automaton<Set<S>, C> convertToDFA(Automaton<S, C> automaton) {
+
     Set<Set<S>> newStates = SubsetHelper.getAllSubsets(automaton.getStates());
-    Set<Set<S>> newAcceptibleStates = findAcceptableStates(newStates, automaton.getAcceptableStates());
+    Set<Set<S>> newAcceptibleStates =
+        findAcceptableStates(newStates, automaton.getAcceptableStates());
     Set<S> initialState = findInitialState(newStates, automaton.getInitialState());
+    Set<C> alphabet = automaton.getAlphabet();
+    TransitionFunction<Set<S>, C> newTransitionFunction =
+        findTransitions(automaton.getTransitionFunction(), newStates, alphabet);
 
-
-
-    return automaton;
+    return new Automaton<Set<S>, C>(newStates, alphabet, newTransitionFunction, initialState,
+        newAcceptibleStates);
 
   }
-  
-  static <S> Set<Set<S>> findAcceptableStates(Set<Set<S>> newStates, Set<S> acceptibleStates){
+
+  static <S> Set<Set<S>> findAcceptableStates(Set<Set<S>> newStates, Set<S> acceptibleStates) {
     Set<Set<S>> newAcceptibleStates = new HashSet<Set<S>>();
-    
-    for(Set<S> newState : newStates){
-     for(S state : newState) {
-       if(acceptibleStates.contains(state)){
-         newAcceptibleStates.add(newState);
-         break;
-       }
-     }
+
+    for (Set<S> newState : newStates) {
+      for (S state : newState) {
+        if (acceptibleStates.contains(state)) {
+          newAcceptibleStates.add(newState);
+          break;
+        }
+      }
     }
-    
+
     return newAcceptibleStates;
-    
+
   }
-  
-  static <S> Set<S> findInitialState(Set<Set<S>> newStates, S initalState){
+
+  static <S> Set<S> findInitialState(Set<Set<S>> newStates, S initalState) {
     Set<S> newInitialState = null;
-    
-    for(Set<S> newState : newStates) {
-      if(newState.equals(new HashSet<S>(Arrays.asList(initalState)))){
-       newInitialState = newState; 
-       break;
+
+    for (Set<S> newState : newStates) {
+      if (newState.equals(new HashSet<S>(Arrays.asList(initalState)))) {
+        newInitialState = newState;
+        break;
       }
     }
     return newInitialState;
   }
 
-  
+  static <S, C> TransitionFunction<Set<S>, C> findTransitions(
+      TransitionFunction<S, C> transitionFunction, Set<Set<S>> states, Set<C> alphabet) {
+    TransitionFunction<Set<S>, C> newTransitionFunction = new TransitionFunction<Set<S>, C>();
+
+    Set<S> newTransition = new HashSet<S>();
+    for (Set<S> state : states) {
+      for (C symbol : alphabet) {
+        newTransition.clear();
+        for (S singleState : state) {
+          newTransition.addAll(transitionFunction.getTransitionResult(singleState, symbol));
+        }
+        newTransitionFunction.addTransition(state, symbol, new HashSet<S>(newTransition));
+      }
+    }
+    return newTransitionFunction;
+  }
+
+
 }
--- src/test/hr/fer/zemris/ppj/automaton/converters/NFAConverterTest.java
+++ src/test/hr/fer/zemris/ppj/automaton/converters/NFAConverterTest.java
@@ -1,6 +1,8 @@
 package hr.fer.zemris.ppj.automaton.converters;
 
 import static org.junit.Assert.*;
+import hr.fer.zemris.ppj.automaton.Automaton;
+import hr.fer.zemris.ppj.automaton.TransitionFunction;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -14,7 +16,7 @@ public class NFAConverterTest {
   public void findAcceptableStatesTest() {
     Set<Integer> acceptibleStates = new HashSet<>();
     acceptibleStates.add(1);
-    
+
     Set<Set<Integer>> states = new HashSet<Set<Integer>>();
     states.add(new HashSet<Integer>(Arrays.asList(0, 3, 4)));
     states.add(new HashSet<Integer>(Arrays.asList(2, 5, 7)));
@@ -23,19 +25,19 @@ public class NFAConverterTest {
     states.add(new HashSet<Integer>(Arrays.asList(2, 5, 4)));
     states.add(new HashSet<Integer>(Arrays.asList(3, 2, 1)));
     states.add(new HashSet<Integer>(Arrays.asList(4, 6, 5)));
-    
+
     Set<Set<Integer>> expectedStates = new HashSet<Set<Integer>>();
     expectedStates.add(new HashSet<Integer>(Arrays.asList(2, 1, 3)));
     expectedStates.add(new HashSet<Integer>(Arrays.asList(3, 2, 1)));
-    
+
     assertEquals(expectedStates, NFAConverter.findAcceptableStates(states, acceptibleStates));
-    
+
   }
-  
+
   @Test
-  public void findInitialState(){
+  public void findInitialState() {
     Set<Set<Integer>> states = new HashSet<Set<Integer>>();
-    
+
     states.add(new HashSet<Integer>(Arrays.asList(1, 2, 3)));
     states.add(new HashSet<Integer>(Arrays.asList(2, 3, 4)));
     states.add(new HashSet<Integer>(Arrays.asList(3, 4, 5)));
@@ -43,9 +45,104 @@ public class NFAConverterTest {
     states.add(new HashSet<Integer>(Arrays.asList(2, 3)));
     states.add(new HashSet<Integer>(Arrays.asList(1)));
     states.add(new HashSet<Integer>(Arrays.asList(2)));
+
+
+    assertEquals(new HashSet<Integer>(Arrays.asList(1)),
+        NFAConverter.findInitialState(states, new Integer(1)));
+
+  }
+
+  @Test
+  public void findTransitionsTest() {
+    TransitionFunction<Integer, Integer> nkaTransitionFunction =
+        new TransitionFunction<Integer, Integer>();
+    nkaTransitionFunction.addTransition(0, 0, 0);
+    nkaTransitionFunction.addTransition(0, 0, 1);
+    nkaTransitionFunction.addTransition(0, 1, 1);
+    nkaTransitionFunction.addTransition(1, 1, 0);
+    nkaTransitionFunction.addTransition(1, 1, 1);
+
+    TransitionFunction<Set<Integer>, Integer> dkaTransitionFunction =
+        new TransitionFunction<Set<Integer>, Integer>();
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0)), 0,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0)), 1,
+        new HashSet<Integer>(Arrays.asList(1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(1)), 0,
+        new HashSet<Integer>(Arrays.asList()));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(1)), 1,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0, 1)), 0,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0, 1)), 1,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList()), 0,
+        new HashSet<Integer>(Arrays.asList()));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList()), 1,
+        new HashSet<Integer>(Arrays.asList()));
+
+    Set<Set<Integer>> dkaStates = new HashSet<Set<Integer>>();
+    dkaStates.add(new HashSet<Integer>(Arrays.asList()));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(0)));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(1)));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(0, 1)));
+
+    assertEquals(
+        dkaTransitionFunction,
+        NFAConverter.findTransitions(nkaTransitionFunction, dkaStates,
+            new HashSet<Integer>(Arrays.asList(0, 1))));
+  }
+
+  @Test
+  public void nfaConverterTest() {
+    TransitionFunction<Integer, Integer> nkaTransitionFunction =
+        new TransitionFunction<Integer, Integer>();
+    nkaTransitionFunction.addTransition(0, 0, 0);
+    nkaTransitionFunction.addTransition(0, 0, 1);
+    nkaTransitionFunction.addTransition(0, 1, 1);
+    nkaTransitionFunction.addTransition(1, 1, 0);
+    nkaTransitionFunction.addTransition(1, 1, 1);
+
+    TransitionFunction<Set<Integer>, Integer> dkaTransitionFunction =
+        new TransitionFunction<Set<Integer>, Integer>();
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0)), 0,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0)), 1,
+        new HashSet<Integer>(Arrays.asList(1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(1)), 0,
+        new HashSet<Integer>(Arrays.asList()));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(1)), 1,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0, 1)), 0,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList(0, 1)), 1,
+        new HashSet<Integer>(Arrays.asList(0, 1)));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList()), 0,
+        new HashSet<Integer>(Arrays.asList()));
+    dkaTransitionFunction.addTransition(new HashSet<Integer>(Arrays.asList()), 1,
+        new HashSet<Integer>(Arrays.asList()));
+
+    Set<Set<Integer>> dkaStates = new HashSet<Set<Integer>>();
+    dkaStates.add(new HashSet<Integer>(Arrays.asList()));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(0)));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(1)));
+    dkaStates.add(new HashSet<Integer>(Arrays.asList(0, 1)));
+
+    Set<Set<Integer>> dkaAcceptableStates = new HashSet<Set<Integer>>();
+    dkaAcceptableStates.add(new HashSet<Integer>(Arrays.asList(1)));
+    dkaAcceptableStates.add(new HashSet<Integer>(Arrays.asList(0, 1)));
+
+    Automaton<Integer, Integer> nka =
+        new Automaton<Integer, Integer>(Arrays.asList(0, 1), Arrays.asList(0, 1),
+            nkaTransitionFunction, 0, Arrays.asList(1));
+
+    Automaton<Set<Integer>, Integer> dka = NFAConverter.convertToDFA(nka);
+
+    Automaton<Set<Integer>, Integer> expectedDKA =
+        new Automaton<Set<Integer>, Integer>(dkaStates, Arrays.asList(0, 1), dkaTransitionFunction,
+            new HashSet<Integer>(Arrays.asList(0)), dkaAcceptableStates);
     
-    
-    assertEquals(new HashSet<Integer>(Arrays.asList(1)), NFAConverter.findInitialState(states, new Integer(1)));
+    assertEquals(expectedDKA, dka);
 
   }
 }
