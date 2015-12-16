d6962c8d605d2abf2607bf49610738c7775a189e
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-12-16 22:20:44 +0100

    Finished on PostfiksIzraz

--- src/main/hr/fer/zemris/ppj/lab3/rules/expression/PostfiksIzraz.java
+++ src/main/hr/fer/zemris/ppj/lab3/rules/expression/PostfiksIzraz.java
@@ -1,5 +1,6 @@
 package hr.fer.zemris.ppj.lab3.rules.expression;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,6 +9,8 @@ import hr.fer.zemris.ppj.lab3.rules.Rule;
 import hr.fer.zemris.ppj.lab3.scope.Scope;
 import hr.fer.zemris.ppj.lab3.types.Array;
 import hr.fer.zemris.ppj.lab3.types.Int;
+import hr.fer.zemris.ppj.lab3.types.NonVoidFunctionType;
+import hr.fer.zemris.ppj.lab3.types.ReturnType;
 import hr.fer.zemris.ppj.lab3.types.Type;
 import hr.fer.zemris.ppj.lab3.types.TypesHelper;
 import hr.fer.zemris.ppj.lab3.types.VoidFunctionType;
@@ -34,7 +37,7 @@ public class PostfiksIzraz extends Rule {
       child.visit(scope);
       node.setType(child.getType());
       node.setlValue(child.islValue());
-      // TODO: check implementation of this second production
+
     } else if (children
         .equals(Arrays.asList("<postfiks_izraz>", "L_UGL_ZAGRADA", "<izraz>", "D_UGL_ZAGRADA"))) {
       SNode postfiks_izraz = node.getChildren().get(0);
@@ -61,11 +64,75 @@ public class PostfiksIzraz extends Rule {
       node.setlValue(!TypesHelper.isArray(X));
     } else if (children.equals(Arrays.asList("<postfiks_izraz>", "L_ZAGRADA", "D_ZAGRADA"))) {
       SNode postfiks_izraz = node.getChildren().get(0);
+
+      // 1
+      postfiks_izraz.visit(scope);
+
+      // 2
+      Type pov = scope.getType(postfiks_izraz.getName());
+      while (scope != null) {
+        if (scope.hasDeclared(postfiks_izraz.getName())) {
+          pov = scope.getType(postfiks_izraz.getName());
+          break;
+        }
+        scope = scope.getParentScope();
+      }
+      if (!postfiks_izraz.getType().equals(new VoidFunctionType((ReturnType) pov))) {
+        throw new SemanticException(getErrorMessage(node));
+      }
+
+      node.setType(pov);
+      node.setlValue(false);
+    } else if (children.equals(
+        Arrays.asList("<postfiks_izraz>", "L_ZAGRADA", "<lista_argumenata>", "D_ZAGRADA"))) {
+      SNode postfiks_izraz = node.getChildren().get(0);
+      SNode lista_argumenata = node.getChildren().get(2);
+
+      // 1
+      postfiks_izraz.visit(scope);
+
+      // 2
+      lista_argumenata.visit(scope);
+
+      // 3 TODO: check this one more time!
+      Type pov = scope.getType(postfiks_izraz.getName());
+      List<Type> params = new ArrayList<>();
+      while (scope != null) {
+        if (scope.hasDeclared(postfiks_izraz.getName())) {
+          pov = scope.getType(postfiks_izraz.getName());
+          for (String name : lista_argumenata.getNames()) {
+            params.add(scope.getType(name));
+          }
+          break;
+        }
+        scope = scope.getParentScope();
+      }
+
+      if (!postfiks_izraz.getType().equals(new NonVoidFunctionType(params, (ReturnType) pov))) {
+        throw new SemanticException(getErrorMessage(node));
+      }
+
+      for (int i = 0; i < params.size(); i++) {
+        if (!TypesHelper.canImplicitlyCast(lista_argumenata.getTypes().get(i), params.get(i))) {
+          throw new SemanticException(getErrorMessage(node));
+        }
+      }
+
+      node.setType(pov);
+      node.setlValue(false);
+    } else if (children.equals(Arrays.asList("<postfiks_izraz>", "OP_INC")) || children.equals(Arrays.asList("<postfiks_izraz>", "OP_DEC"))) {
+      SNode postfiks_izraz = node.getChildren().get(0);
       
       // 1
       postfiks_izraz.visit(scope);
       
       // 2
+      if (!postfiks_izraz.islValue() || !TypesHelper.canImplicitlyCast(postfiks_izraz.getType(), Int.INT)) {
+        throw new SemanticException(getErrorMessage(node));
+      }
+      
+      node.setType(Int.INT);
+      node.setlValue(false);
     }
   }
 
