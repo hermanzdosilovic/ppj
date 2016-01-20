3bd96baf42f7067cbdde70bb66754d90444f51fd
Ivan Krpelnik <ikrpelnik1@gmail.com>
2016-01-20 14:06:45 +0100

    Dodane funkcije za generiranje labela

--- src/main/hr/fer/zemris/ppj/lab4/GeneratorKoda.java
+++ src/main/hr/fer/zemris/ppj/lab4/GeneratorKoda.java
@@ -9,7 +9,8 @@ import hr.fer.zemris.ppj.lab3.analyzer.SemantickiAnalizator;
 
 public class GeneratorKoda {
   private static BufferedWriter fileWriter;
-
+  private static int counter = 0;
+  
   public static void main(String[] args) throws IOException {
     fileWriter = new BufferedWriter(new FileWriter(new File("a.frisc")));
     writeln("\tMOVE 40000, R7");
@@ -46,4 +47,33 @@ public class GeneratorKoda {
   public static void writeln(String line) {
     write(line + "\n");
   }
+  
+  /**
+   * Takes a name of a global variable and returns a label representing that
+   * global variable in FRISC code. 
+   * @param name - name of a global variable
+   * @return label representing provided variable
+   */
+  public static String getGlobalVariableLabel(String name) {
+    return "G_" + name;
+  }
+  
+  /**
+   * Takes a name of a function and returns a label representing
+   * that function in FRISC code.
+   * @param name - function name
+   * @return label representing provided function name
+   */
+  public static String getFunctionLabel(String name) {
+    return "F_" + name;
+  }
+  
+  /**
+   * Generates a new label with unique name.
+   * @return new label
+   */
+  public static String getNextLabel() {
+    return "L_" + counter;
+  }
+
 }
