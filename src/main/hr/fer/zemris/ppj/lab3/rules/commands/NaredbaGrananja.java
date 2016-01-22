22667f3d9c0421cf0ca2f11bc22cf71847e8b01c
Josipa Kelava <kelava.josipa@gmail.com>
2016-01-22 23:58:09 +0100

    Dodan ispis za naredbe grananja

--- src/main/hr/fer/zemris/ppj/lab3/rules/commands/NaredbaGrananja.java
+++ src/main/hr/fer/zemris/ppj/lab3/rules/commands/NaredbaGrananja.java
@@ -3,11 +3,13 @@ package hr.fer.zemris.ppj.lab3.rules.commands;
 import java.util.Arrays;
 import java.util.List;
 
+
 import hr.fer.zemris.ppj.lab3.analyzer.SemanticException;
 import hr.fer.zemris.ppj.lab3.rules.Rule;
 import hr.fer.zemris.ppj.lab3.scope.Scope;
 import hr.fer.zemris.ppj.lab3.types.Int;
 import hr.fer.zemris.ppj.lab3.types.TypesHelper;
+import hr.fer.zemris.ppj.lab4.GeneratorKoda;
 import hr.fer.zemris.ppj.node.SNode;
 import hr.fer.zemris.ppj.symbol.NonTerminalSymbol;
 
@@ -29,15 +31,35 @@ public class NaredbaGrananja extends Rule {
       if (!TypesHelper.canImplicitlyCast(izraz.getType(), Int.INT)) {
         throw new SemanticException(getErrorMessage(node));
       }
+      String labela1 = GeneratorKoda.getNextLabel();
+      
+      GeneratorKoda.writeln("\tPOP R0");
+      GeneratorKoda.writeln("\tCMP R0, 0");
+      GeneratorKoda.writeln("\tJP_EQ " + labela1);
+      
       node.getChildren().get(4).visit(scope);
+      GeneratorKoda.writeln(labela1);
+    
     } else if (childrenValues.equals(Arrays.asList("KR_IF", "L_ZAGRADA", "<izraz>", "D_ZAGRADA",
         "<naredba>", "KR_ELSE", "<naredba>"))) {
       izraz.visit(scope);
       if (!TypesHelper.canImplicitlyCast(node.getChildren().get(2).getType(), Int.INT)) {
         throw new SemanticException(getErrorMessage(node));
       }
+      
+      String labela1 = GeneratorKoda.getNextLabel();
+      String labela2 = GeneratorKoda.getNextLabel();
+      
+      GeneratorKoda.writeln("\tPOP R0");
+      GeneratorKoda.writeln("\tCMP R0, 0");
+      GeneratorKoda.writeln("\tJP_EQ " + labela1);
+      
       node.getChildren().get(4).visit(scope);
+      GeneratorKoda.writeln("\tJP "+ labela2);
+      
+      GeneratorKoda.writeln(labela1);
       node.getChildren().get(6).visit(scope);
+      GeneratorKoda.writeln(labela2);
     }
   }
 
